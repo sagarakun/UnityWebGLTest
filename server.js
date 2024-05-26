2f15d927e79713ed68d5e352eac76ddba9cab37a
kosuke sagara <nukaragas@gmail.com>
2024-05-26 17:45:46 +0900

    Update CORS Anywhere settings

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const corsAnywhere = require('cors-anywhere');
 
-const app = express();
 const host = process.env.HOST || '0.0.0.0';
 const port = process.env.PORT || 8080;
 
 corsAnywhere.createServer({
-  originWhitelist: [], // Allow all origins
-  requireHeader: [],
-  removeHeaders: []
+  originWhitelist: ['*'], // Allow all origins
+  requireHeader: ['origin', 'x-requested-with'],
+  removeHeaders: ['cookie', 'cookie2']
 }).listen(port, host, function () {
   console.log('Running CORS Anywhere on ' + host + ':' + port);
 });
